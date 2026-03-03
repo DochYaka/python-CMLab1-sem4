d11b3447da57080735fc1d81defa51c43b4bc8c9
DochYaka <dmitrijkiselev194@gmail.com>
2026-03-04 01:27:24 +0700

    F I X

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import graphCreate
 
 
 def main():
-    # graphCreate.Graph()
+    graphCreate.Graph()
 
     intervals = [(-4, -3), (-2, -1), (0, 1), (4, 5)]
 
-    # print()
+    print()
 
-    # print("Метод Ньютона\n")
-    # for a, b in intervals:
-    #     newtonMethod.newton_method(a, b)
-    #     print()
+    print("Метод Ньютона\n")
+    for a, b in intervals:
+        newtonMethod.newton_method(a, b)
+        print()
 
-    # print()
+    print()
 
-    # print("Метод простых итераций\n")
-    # for a, b in intervals:
-    #     simpleIterationMethod.simple_iteration_method(a, b)
-    #     print()
+    print("Метод простых итераций\n")
+    for a, b in intervals:
+        simpleIterationMethod.simple_iteration_method(a, b)
+        print()
 
     print()
 
